acf6491622f9b12da737c9f0df79a6ccfd61ca08
mendasriharinaidu <mendasriharinaidu@gmail.com>
2026-07-27 18:02:54 +0530

    Update sample_coefficient_memory.v

--- FIR_memory/sample_coefficient_memory.v
+++ FIR_memory/sample_coefficient_memory.v
@@ -7,17 +7,17 @@ wire [7:0]wr_point,rd_point;
 wire rd;
 input [7:0]data_in1,data_in2;
 output reg [7:0]data_out1,data_out2;
-reg[7:0]mem1[0:M-1];
-reg[7:0]mem2[0:N-1];
+reg[7:0]mem1[0:M-1];               // sample memory
+reg[7:0]mem2[0:N-1];               // coefficient memory
 
 assign rd=(wr_addr>0)?1:0;
 
-wr_counter a(clk,wr_addr);
-rd_counter b(done,rd,rd_addr);
-wr_pointer c(clk,wr_point);
-rd_pointer d(done,rd,rd_point);
+sample_mem_wr_pointer a(clk,wr_addr);
+sample_mem_rd_pointer b(done,rd,rd_addr);
+coefficient_mem_wr_pointer c(clk,wr_point);
+coefficient_mem_rd_pointer d(done,rd,rd_point);
 
-always@(posedge clk)
+always@(posedge clk)   
 begin
 mem1[wr_addr]<=data_in1;
 mem2[wr_point]<=data_in2;
