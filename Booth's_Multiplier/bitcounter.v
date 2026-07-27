d95a08b11d148280e80cd2e439df169d799ef11e
mendasriharinaidu <mendasriharinaidu@gmail.com>
2026-07-27 18:49:19 +0530

    Create bitcounter.v

---
+++ Booth's_Multiplier/bitcounter.v
@@ -0,0 +1,13 @@
+module bitcounter(countdata,decr,ldcount,clk);
+     input decr,ldcount,clk;
+	 output reg [3:0] countdata;
+	 
+	 always@(posedge clk)
+	   begin
+	       if(ldcount)
+		      countdata <= 8; //since datain is of 16 bits width
+		   else if(decr)
+              countdata <= countdata - 1;			
+	   end
+	   
+endmodule
