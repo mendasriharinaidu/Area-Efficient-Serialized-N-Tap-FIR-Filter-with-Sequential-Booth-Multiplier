ee6d5d1b389fb86eab693627b84a50463cedf90b
mendasriharinaidu <mendasriharinaidu@gmail.com>
2026-07-27 17:59:14 +0530

    Update coefficient_mem_rd_pointer.v

--- FIR_memory/coefficient_mem_rd_pointer.v
+++ FIR_memory/coefficient_mem_rd_pointer.v
@@ -1,1 +1,25 @@
+module coefficient_mem_rd_pointer(clk,rd,addr);
+parameter N=21,M=240;
+input clk;
+input rd;
+output reg [7:0]addr=0;
+reg [7:0]p=0;
+always@(posedge clk)
+if(rd)
+if(addr<N-1)
+addr<=addr+1;
+else if(addr==N-1)
+begin
+p<=p+1;
+if(p==M-N)
+begin
+addr<=N-1;
+p<=M-N;
+end
+else
+addr<=0;
+end
+else
+addr<=0;
+endmodule
 
